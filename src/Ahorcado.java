d34ba23ec035394c89422c4e2fc8c5aaedce26dc
nibal.ink <anibalcaeiro@gmail.com>
2024-08-03 21:31:46 -0300

    first commit: ahorcado en java

---
+++ src/Ahorcado.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int numero_entero = 9;
+
+        System.out.println(numero_entero);
+
+    }
+}
+
